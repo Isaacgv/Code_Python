ff5f6eb7a269c4620ff4e74077b2a495a47fa64d
Isaac <isaacgonzalesv@gmail.com>
2020-07-05 18:18:50 -0300

    dev: Django REST API and ORM

---
+++ codenation/python-10/restapi/api/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from collections import Counter
+
+@api_view(["POST"])
+def lambda_function(request):
+
+    data = request.data.get('question')
+    data =list(Counter(data).elements())
+    solution = sorted(data, key=data.count, reverse=True)
+    return Response({'solution': solution}) 
